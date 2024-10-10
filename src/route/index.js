10f0bb4b1850673c6696e4b56cef6846d858165e
cui_yu_lin <1239303157@qq.com>
2024-10-10 20:25:38 +0800

    修改重定向路径

--- src/route/index.js
+++ src/route/index.js
@@ -1,28 +1,30 @@
-import { createWebHistory, createRouter } from 'vue-router'
-import HomePage from '@/views/children/HomePage.vue'
-import UserLogin from '@/views/UserLogin.vue'
-import LayoutView from '@/layout/layout.vue'
-import CodeRepository from '@/views/children/CodeRepository.vue'
+import { createWebHistory, createRouter } from "vue-router";
+import HomePage from "@/views/children/HomePage.vue";
+import UserLogin from "@/views/UserLogin.vue";
+import LayoutView from "@/layout/layout.vue";
+import CodeRepository from "@/views/children/CodeRepository.vue";
 
 const routes = [
-    {    path: '/',
-         component: LayoutView ,
-         children:[
-        {
-            path:"/home",
-            component:HomePage
-        },
-        {
-          path:"/coderepository",
-          component:CodeRepository
-      }
-    ]},
-    { path: '/login', component: UserLogin },
-  ]
+  { path: "/", component: UserLogin },
+  {
+    path: "/layout",
+    component: LayoutView,
+    children: [
+      {
+        path: "/home",
+        component: HomePage,
+      },
+      {
+        path: "/coderepository",
+        component: CodeRepository,
+      },
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
 export default router;
